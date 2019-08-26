dd51425d15b8fc6ade3c915eb1f5afaed9403c86
Ezequiel Leites <ezequiel@leites.dev>
2019-08-26 17:41:27 +0200

    Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,46 +5,69 @@ import { SafeAreaView } from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 // Hooks
 import { useNavigation } from '../Navigation/hooks';
+// Helpers
+import { isLogged, removeSession } from '../../helpers/session';
 
-const menuItems = [
-    { icon: 'ios-calendar', name: 'Eventos', url: 'Events' },
-    { icon: 'ios-home', name: 'Locales', url: 'Stores' },
-];
+const MenuItem = menuItem => (
+    <TouchableOpacity
+        onPress={ menuItem.onPress }
+        style={{
+            alignItems: 'center',
+            flexDirection: 'row',
+            height: 40,
+            marginBottom: 4,
+            padding: 8,
+        }}
+    >
+        <Icon name={ menuItem.icon } size={ 24 } />
+        <Text style={{ fontFamily: 'Futura', fontSize: 14, marginLeft: 16 }}>{ menuItem.name }</Text>
+    </TouchableOpacity>
+)
+
+const BottomItem = menuItem => (
+    <TouchableOpacity
+        onPress={ menuItem.onPress }
+        style={{ height: 40, justifyContent: 'center' }}
+    >
+        <Text
+            style={{
+                fontFamily: 'Futura',
+                fontSize: 20,
+                textAlign: 'center',
+            }}
+        >
+            { menuItem.name.toUpperCase() }
+        </Text>
+    </TouchableOpacity>
+);
 
 const Menu = () => {
+    const bottomItems = [];
     const { goTo } = useNavigation();
+    const logout = () => {
+        removeSession();
+        goTo('Events');
+    }
+
+    const menuItems = [
+        { icon: 'ios-calendar', name: 'Eventos', onPress: () => goTo('Events') },
+        { icon: 'ios-home', name: 'Locales', onPress: () => goTo('Stores') },
+    ];
+
+    if (isLogged()) {
+        bottomItems.push({ name: 'Salir', onPress: logout });
+        menuItems.push()
+    } else {
+        bottomItems.push({ name: 'Ingresar', onPress: () => goTo('Login') });
+    }
 
     return (
         <SafeAreaView style={{ height: '100%', padding: 12, width: '100%' }}>
             <View style={{ flex: 1 }}>
-                { menuItems.map((menuItem, index) =>
-                    <TouchableOpacity
-                        key={ index }
-                        onPress={ goTo(menuItem.url) }
-                        style={{
-                            alignItems: 'center',
-                            flexDirection: 'row',
-                            height: 40,
-                            marginBottom: 4,
-                            padding: 8,
-                        }}
-                    >
-                        <Icon name={ menuItem.icon } size={ 24 } />
-                        <Text style={{ fontFamily: 'Futura', fontSize: 14, marginLeft: 16 }}>{ menuItem.name }</Text>
-                    </TouchableOpacity>
-                )}
+                { menuItems.map((menuItem, index) => <MenuItem key={ index } { ...menuItem } />) }
             </View>
             <View style={{ height: 40 }}>
-                <TouchableOpacity
-                    onPress={ goTo('Login') }
-                    style={{ height: 40, justifyContent: 'center' }}
-                >
-                    <Text style={{
-                        fontFamily: 'Futura',
-                        fontSize: 20,
-                        textAlign: 'center',
-                    }}>INGRESAR</Text>
-                </TouchableOpacity>
+                { bottomItems.map((menuItem, index) => <BottomItem key={ index } { ...menuItem } />) }
             </View>
         </SafeAreaView>
     );
---
+++ src/helpers/session.js
@@ -0,0 +1,18 @@
+// Dependencies
+import AsyncStorage from '@react-native-community/async-storage';
+
+const SESSION_KEY = 'ygoArgentinaSession';
+
+export const setSession = user => AsyncStorage.setItem(SESSION_KEY, JSON.stringify(user));
+export const removeSession = () => AsyncStorage.removeItem(SESSION_KEY);
+
+export const getSession = async () => {
+    const session = await AsyncStorage.getItem(SESSION_KEY);
+    if (!session) return null;
+    return JSON.parse(session);
+}
+
+export const isLogged = async() => {
+    const session = await getSession();
+    return session;
+}
