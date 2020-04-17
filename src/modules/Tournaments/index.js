9883bc5b3a184e9d76478078c99e2c1cc54adb7b
Ezequiel Leites <ezequiel@leites.dev>
2020-04-17 02:35:35 +0200

    feat: add tournaments page

--- src/modules/Tournaments/index.js
+++ src/modules/Tournaments/index.js
@@ -1,7 +1,10 @@
 // Dependencies
 import React from 'react';
+import dayjs from 'dayjs';
 import {Text, Image, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 // Components
+import TextBadge from '#components/TextBadge';
 import Layout from '#components/Layout';
 // Contexts
 import {useLoader} from '#contexts/Loader';
@@ -10,13 +13,14 @@ import styles from './styles';
 // Services
 import {all} from '#services/tournaments';
 // Utils
-import {parseTournamentState} from '#utils/challonge';
+import {getCurrentRound, parseTournamentState} from '#utils/challonge';
 // Images
 import Logo from '#images/logo.png';
 
 const Tournaments = () => {
   const [tournaments, setTournaments] = React.useState(null);
   const {isLoading, showLoader, hideLoader} = useLoader();
+  const currentRound = tournament => getCurrentRound(tournament);
 
   const fetchTournaments = () => {
     all()
@@ -35,12 +39,10 @@ const Tournaments = () => {
           key={tournament.id}
           style={{
             flexDirection: 'row',
-            height: 100,
-            marginBottom: 10,
+            marginBottom: 14,
           }}>
           <View
             style={{
-              height: 100,
               alignItems: 'center',
               justifyContent: 'center',
               width: 100,
@@ -52,29 +54,74 @@ const Tournaments = () => {
             />
           </View>
           <View style={{flex: 1, padding: 4}}>
-            <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 4}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 8}}>
               {tournament.title}
-            </Text>
-            <Text style={{fontSize: 16, marginBottom: 4}}>
-              Jugadores {tournament.players.length}
             </Text>
             <View
               style={{
-                borderWidth: 1,
-                padding: 4,
-                borderRadius: 4,
-                alignSelf: 'flex-start',
+                alignItems: 'center',
+                flexDirection: 'row',
+                marginBottom: 8,
               }}>
+              <Icon size={24} name="md-people" />
               <Text
-                style={{
-                  fontSize: 14,
-                  borderColor: 'black',
-                }}>
-                {parseTournamentState(
-                  tournament.challonge.state,
+                style={{fontSize: 20, marginLeft: 6, flex: 1, marginBottom: 4}}>
+                {tournament.players.length}
+              </Text>
+              <TextBadge
+                variant="info"
+                label={parseTournamentState(
+                  tournament?.challonge?.state,
                   tournament,
                 ).toUpperCase()}
-              </Text>
+              />
+            </View>
+            <View style={{alignItems: 'center', flexDirection: 'row'}}>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                {tournament.rounds > 0 && (
+                  <>
+                    <Icon size={24} name="logo-game-controller-b" />
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        marginHorizontal: 6,
+                        marginBottom: 4,
+                      }}>
+                      {tournament.rounds}
+                    </Text>
+                    {!!tournament.top && (
+                      <>
+                        <Icon size={24} name="ios-podium" />
+                        <Text style={{fontSize: 20, marginLeft: 6, flex: 1}}>
+                          {tournament.top.players.length}
+                        </Text>
+                      </>
+                    )}
+                  </>
+                )}
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end',
+                }}>
+                {dayjs().isBefore(dayjs(tournament.dateFrom)) ? (
+                  <>
+                    <Icon size={24} name="md-time" />
+                    <Text style={{fontSize: 20, marginLeft: 6}}>
+                      {dayjs(tournament.dateFrom).format('DD/MM HH:mm')}
+                    </Text>
+                  </>
+                ) : (
+                  <>
+                    <Icon size={24} name="ios-calendar" />
+                    <Text style={{fontSize: 20, marginLeft: 6}}>
+                      {dayjs(tournament.dateFrom).format('DD/MM/YY')}
+                    </Text>
+                  </>
+                )}
+              </View>
             </View>
           </View>
         </View>
